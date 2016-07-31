7c02f797bed4a67d9a041dd52bc07a9d7437c892
Igor Kapkov <igasgeek@me.com>
2016-07-31 10:09:36 +1000

    Update fry.rb

--- fry.rb
+++ fry.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fry < Formula
   homepage 'https://github.com/terlar/fry#readme'
   url 'https://github.com/terlar/fry/archive/0.1.5.tar.gz'
-  sha256 '24222bd50ebc024507bb3e404789513af3c64b25'
+  sha256 '5700e01ff4fe41fcfa307af42e8ec843b4be39adc7c8b227ebc73d372aa05c43'
 
   head 'https://github.com/terlar/fry.git'
 
